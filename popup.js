67096687db9060435c510055330cbe007e1e946a
Nazariy995 <nazariy1995@gmail.com>
2020-07-27 17:17:10 -0400

    ADD: First Commit

---
+++ popup.js
@@ -0,0 +1,30 @@
+var circle = new ProgressBar.Circle('#progress-bar', {
+    color: '#aaa',
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+      },
+      from: { color: '#aaa', width: 1 },
+      to: { color: '#333', width: 4 },
+      // Set default step function for all animate calls
+      step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+    
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+          circle.setText('');
+        } else {
+          circle.setText(value);
+        }
+    
+      }
+});
+
+circle.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+circle.text.style.fontSize = '2rem';
+
+circle.animate(1.0);  
